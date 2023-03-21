68dc5308401aa6edf4852e2fac4d6dc88bb173ac
Sai-nath <sai.sai.sn@gmail.com>
2023-03-22 03:25:12 +0530

    code changes4

--- index.js
+++ index.js
@@ -80,36 +80,22 @@ app.post("/webhook",(req,res)=>{ //i want some
                console.log("from "+from);
                console.log("boady param "+msg_body);
 
-           
-axios({
-  method: 'POST',
-  url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-  data: {
-    messaging_product: 'whatsapp',
-    recipient: {
-      id: from
-    },
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: menu.body,
-          buttons: menu.options.map(option => {
-            return {
-              type: 'postback',
-              title: option.text,
-              payload: option.value
-            };
-          })
-        }
-      }
-    }
-  },
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                        body: "👋 Welcome to Hitpa! Please select an option:\n\n📝 1. Policy Data\n💳 2. Ecard\n📋 3. Claim Status\n🏠 4. Main Menu\n👋 5. Exit,your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
                res.sendStatus(200);
             }else{
                 res.sendStatus(404);
