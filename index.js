a28fc68293cbc138b28f38c8ee9ddbb1c7ec2868
Sai-nath <sai.sai.sn@gmail.com>
2023-03-24 01:12:09 +0530

    code changes15

--- index.js
+++ index.js
@@ -111,8 +111,9 @@ app.post("/webhook",(req,res)=>{ //i want some
                console.log("phone number "+phon_no_id);
                console.log("from "+from);
                console.log("boady param "+msg_body);
-               if(msg_body===1)
+               if(msg_body==='1')
                {
+                console.log("inside body msg_body 1");
                 app.get("/getpolicydetails",(req,res)=>{
                     const axios = require('axios');
                     console.log("inside body getpolicydetails");
